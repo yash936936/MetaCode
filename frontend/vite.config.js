f0d856861e12464e39d3018718a9e5479b53a2f9
yash936936 <yashm15082005@gmail.com>
2026-01-05 00:29:36 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,5 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/',
 })
